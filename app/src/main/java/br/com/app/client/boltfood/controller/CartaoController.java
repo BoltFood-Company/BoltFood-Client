e46d793bb2e086b829d1024a2ac1fc9d7fa07404
Philipe Cunha <philipe.cunha92@gmail.com>
2019-04-12 21:29:40 -0300

    inclusão CartaoDAO e CartaoController

--- app/src/main/java/br/com/app/client/boltfood/controller/CartaoController.java
+++ app/src/main/java/br/com/app/client/boltfood/controller/CartaoController.java
@@ -1,4 +1,17 @@
 package br.com.app.client.boltfood.controller;
 
+import br.com.app.client.boltfood.dao.CartaoDAO;
+import br.com.app.client.boltfood.model.entity.Cartao;
+
 public class CartaoController {
+
+    private CartaoDAO cartaoDAO;
+
+    public CartaoController(){
+        cartaoDAO = new CartaoDAO();
+    }
+
+    public void inserirCartao(Cartao cartao) {
+        cartaoDAO.inserirCartao(cartao);
+    }
 }
--- app/src/main/java/br/com/app/client/boltfood/dao/CartaoDAO.java
+++ app/src/main/java/br/com/app/client/boltfood/dao/CartaoDAO.java
@@ -1,4 +1,18 @@
 package br.com.app.client.boltfood.dao;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import br.com.app.client.boltfood.model.entity.Cartao;
+
 public class CartaoDAO {
+
+    private FirebaseFirestore db;
+
+    public CartaoDAO() {
+        db = FirebaseFirestore.getInstance();
+    }
+
+    public void inserirCartao(Cartao cartao) {
+
+    }
 }
