a2baf13330427ada6eb53ade03254af08abd11ef
Felipe <felipe.negri43@gmail.com.br>
2019-05-25 20:22:07 -0300

    criacao do layout da tela de resumo de pedido e inicio da populacao da mesma

--- app/src/main/java/br/com/app/client/boltfood/view/ResumoPedidoActivity.java
+++ app/src/main/java/br/com/app/client/boltfood/view/ResumoPedidoActivity.java
@@ -2,11 +2,17 @@ package br.com.app.client.boltfood.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import br.com.app.client.boltfood.R;
 
 public class ResumoPedidoActivity extends AppCompatActivity {
+
+    private TextView nome;
+    private TextView total;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +25,19 @@ public class ResumoPedidoActivity extends AppCompatActivity {
 
         Bundle extras = getIntent().getExtras();
 
+        nome = findViewById(R.id.nomeResumoPedido);
+        total = findViewById(R.id.precoResumoPedido);
+
         if(extras != null) {
             String idPedido = (String) extras.get("idPedido");
             String nomeRestaurante = (String) extras.get("nomeRestaurante");
             String urlRestaurante = (String) extras.get("urlRestaurante");
             String totalPedido = (String) extras.get("totalPedido");
+
+            nome.setText(nomeRestaurante);
+            total.setText("Total: " + totalPedido);
+
+
         }
     }
 
