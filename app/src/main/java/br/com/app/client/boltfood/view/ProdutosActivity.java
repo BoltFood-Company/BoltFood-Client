fa3260a456a7230b0023d67bb9640b3f789bbd38
felipe <felipe.negri43@gmail.com>
2019-06-04 19:08:44 -0300

    ajuste para cards de restaurante carregarem a imagem em circulo

--- app/src/main/java/br/com/app/client/boltfood/view/ProdutosActivity.java
+++ app/src/main/java/br/com/app/client/boltfood/view/ProdutosActivity.java
@@ -110,7 +110,7 @@ public class ProdutosActivity extends AppCompatActivity implements SearchView.On
             headerNome.setText(nomeRestaurante);
             headerEstrela.setText(notaRestaurante);
             Glide.with(getApplicationContext()).load(imagemRestaurante).into(headerImagem);
-            Glide.with(this).load(bgRestaurante).into(new SimpleTarget<Drawable>() {
+            Glide.with(this).load(bgRestaurante).centerCrop().into(new SimpleTarget<Drawable>() {
                 @Override
                 public void onResourceReady(@NonNull Drawable resource, @Nullable Transition<? super Drawable> transition) {
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
